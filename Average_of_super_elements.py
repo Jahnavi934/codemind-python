1a25f16fe5324ff124586c4647cc890ba76109b2
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2022-12-07 14:50:29 +0530

    n=int(input()) arr=list(map(int,input().split())) d=[] flag=0 for i in range(len(arr)):     if (arr.count(arr[i])==arr[i]):         if arr[i] not in d:             d.append(arr[i])             flag=1 if flag==1:     print("{:.2f}".format((sum(d)/len(d)))) else:     print("-1")

---
+++ Average_of_super_elements.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+d=[]
+flag=0
+for i in range(len(arr)):
+    if (arr.count(arr[i])==arr[i]):
+        if arr[i] not in d:
+            d.append(arr[i])
+            flag=1
+if flag==1:
+    print("{:.2f}".format((sum(d)/len(d))))
+else:
+    print("-1")
