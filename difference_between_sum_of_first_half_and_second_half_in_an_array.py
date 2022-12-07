91b4d616fbff583c6c9dce7a8091357dee456174
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2022-12-07 14:21:55 +0530

    n=int(input()) arr=list(map(int,input().split())) d=[] f=[] for i in range(len(arr)//2):     d.append(arr[i]) for i in range(len(arr)//2,len(arr)):     f.append(arr[i]) print(abs(sum(d)-sum(f)))

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,9 @@
+n=int(input())
+arr=list(map(int,input().split()))
+d=[]
+f=[]
+for i in range(len(arr)//2):
+    d.append(arr[i])
+for i in range(len(arr)//2,len(arr)):
+    f.append(arr[i])
+print(abs(sum(d)-sum(f)))
