87be7112b3bb3cbbda71ef36b22fac14af30115e
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2022-12-19 15:49:01 +0530

    n=int(input()) d=list(map(int,input().split())) a,b=map(int,input().split()) c=[] for i in d:     if i>=a and i<=b:         c.append(i) if len(c)==0:     print("-1") else:     print(min(c))

---
+++ minimum_between_A_and_B.py
@@ -0,0 +1,12 @@
+n=int(input())
+d=list(map(int,input().split()))
+a,b=map(int,input().split())
+c=[]
+for i in d:
+    if i>=a and i<=b:
+        c.append(i)
+if len(c)==0:
+    print("-1")
+else:
+    print(min(c))
+        
